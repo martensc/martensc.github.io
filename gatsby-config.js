3642650371582cee4ba6f2e17722b0f933b10650
Carl Martens <carlrmartens@gmail.com>
2022-12-31 17:08:57 -0600

    Removed unused packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,30 +7,33 @@ module.exports = {
     siteUrl: `https://carl-martens.com`
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        "trackingId": "UA-11631620-1"
+      }
+    },
     "gatsby-plugin-cname",
     "gatsby-plugin-postcss",
-    "gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "UA-11631620-1"
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    },
+    {
+      resolve: "gatsby-omni-font-loader",
+      options: {
+        enableListener: true,
+        preconnect: ["https://fonts.googleapis.com", "https://fonts.gstatic.com"],
+        web: [
+          {
+            name: "Outfit",
+            file: "https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap",
+          }
+        ],
+      }
     }
-  }, "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  },
-  {
-    resolve: "gatsby-omni-font-loader",
-    options: {
-      enableListener: true,
-      preconnect: ["https://fonts.googleapis.com", "https://fonts.gstatic.com"],
-      web: [
-        {
-          name: "Outfit",
-          file: "https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap",
-        }
-      ],
-    }
-  }]
+  ]
 };
