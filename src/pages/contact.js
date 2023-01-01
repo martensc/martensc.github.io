6b778312e936eb65a6079cbfb53750ddc084d026
Carl Martens <carlrmartens@gmail.com>
2023-01-01 13:13:14 -0600

    removed border radius on ios inputs and buttons

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,23 +17,23 @@ const ContactPage = () => {
           <label className="block" htmlFor="name">
             Full Name
           </label>
-          <input className="mt-4 mb-8 block w-full sm:w-1/2 py-2 bg-transparent border-b-2 border-slate-900 dark:border-slate-100 text-lg placeholder-slate-400 focus:outline-none focus:border-sky-500" id="name" type="text" name="name" />
+          <input className="mt-4 mb-8 block w-full sm:w-1/2 py-2 bg-transparent border-b-2 rounded-none border-slate-900 dark:border-slate-100 text-lg placeholder-slate-400 focus:outline-none focus:border-sky-500" id="name" type="text" name="name" />
         </div>
         <div className="form-row">
           <label className="block" htmlFor="email">
             Email
           </label>
-          <input className="mt-4 mb-8 block w-full sm:w-1/2 py-2 bg-transparent border-b-2 border-slate-900 dark:border-slate-100 text-lg placeholder-slate-400 focus:outline-none focus:border-sky-500" id="email" type="email" name="_replyto" />
+          <input className="mt-4 mb-8 block w-full sm:w-1/2 py-2 bg-transparent border-b-2 rounded-none border-slate-900 dark:border-slate-100 text-lg placeholder-slate-400 focus:outline-none focus:border-sky-500" id="email" type="email" name="_replyto" />
         </div>
         <div className="form-row">
           <label className="block" htmlFor="message">
             Message
           </label>
-          <textarea className="mt-4 mb-16 block w-full sm:w-3/4 py-2 bg-transparent border-b-2 border-slate-900 dark:border-slate-100 text-lg placeholder-slate-400 focus:outline-none focus:border-sky-500" id="message" type="text" name="message" />
+          <textarea className="mt-4 mb-16 block w-full sm:w-3/4 py-2 bg-transparent border-b-2 rounded-none border-slate-900 dark:border-slate-100 text-lg placeholder-slate-400 focus:outline-none focus:border-sky-500" id="message" type="text" name="message" />
         </div>
         <input type="text" name="_gotcha" style={formStyles} />
         <div className="form-row">
-          <input className="cursor-pointer bg-slate-900 dark:bg-slate-100 border-slate-900 dark:border-slate-100 border-2 text-white dark:text-slate-900 py-4 px-8 hover:bg-transparent hover:text-slate-900 dark:hover:text-slate-100 focus:bg-transparent focus:text-slate-900" type="submit" value="Send" />
+          <input className="cursor-pointer bg-slate-900 dark:bg-slate-100 border-slate-900 dark:border-slate-100 border-2 rounded-none text-white dark:text-slate-900 py-4 px-8 hover:bg-transparent hover:text-slate-900 dark:hover:text-slate-100 focus:bg-transparent focus:text-slate-900" type="submit" value="Send" />
         </div>
         <input type="hidden" name="_next" value="/thank-you" />
       </form>
